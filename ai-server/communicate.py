6340ab902966d1a2fe32b1546be5f252ea0f5a7d
fhtj12 <fhtj12@gmail.com>
2018-10-17 15:16:25 +0900

    [AI서버] 통신 모듈 프로토타입 및 각 기능별 검색 옵션 파일만 생성

--- ai-server/communicate.py
+++ ai-server/communicate.py
@@ -1,5 +1,19 @@
 # 메인 - 통신
 
-from data.collect import search_location_by_tag
+import http.server
 
-print(search_location_by_tag("은평구 맛집", "1"))
+from exception.error import error
+
+class Handler(http.server.BaseHTTPRequestHandler) : 
+    def do_GET(self) : 
+        # 요청이 get일 때 실행되고, 응답 메세지를 전송한다.
+        self.send_response(200)
+        self.wfile.write(bytes(error.common_errors["ok"], 'utf-8'))
+
+# 요청받을 주소
+address = ('127.0.0.1', 19503)
+
+# 요청 대기
+listener = http.server.HTTPServer(address, Handler)
+print(f'http://{address[0]}:{address[1]} 주소에서 요청 대기중...')
+listener.serve_forever()
